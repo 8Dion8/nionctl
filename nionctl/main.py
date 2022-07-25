c62f6019c110e8cb951014766b27f296ca24e93d
8Dion8 <shvartserinfo@gmail.com>
2022-07-25 17:11:41 +0300

    0.2.1 Added rich styling

--- nionctl/main.py
+++ nionctl/main.py
@@ -2,6 +2,9 @@ import typer
 import os
 import subprocess
 from time import sleep
+from rich import print as rich_print
+from rich.console import Console
+from rich.table import Table
 
 def run_shell_command(command):
     out = subprocess.run([i for i in command.split()], stdout=subprocess.PIPE).stdout.decode('utf-8')
@@ -15,6 +18,8 @@ bluetooth_app = typer.Typer()
 main_app.add_typer(wifi_app, name="wifi")
 main_app.add_typer(bluetooth_app, name="bt")
 
+console = Console()
+
 HOMEDIR = os.getenv("HOME")
 
 
@@ -24,7 +29,7 @@ HOMEDIR = os.getenv("HOME")
 def gitclone(link: str, dest: str = HOMEDIR + "/gitclones"):
     os.system(f"cd {dest}")
     out = run_shell_command(f"git clone {link}")
-    print(out)
+    rich_print(out)
     
 @main_app.command()
 def neofetch():
@@ -36,18 +41,46 @@ def neofetch():
 @wifi_app.command("list")
 def wifi_list():
     out = run_shell_command("nmcli dev wifi")
-    print(out)
+    
+    table = Table("BSSID", "Name", "Channel", "Rate", "Signal", "Security", show_lines=True)
+
+    first_line = True
+
+    for line in out.splitlines():
+        wifi_inuse = False
+        if first_line:
+            first_line = False
+            continue
+        elements = list(filter(None, line.split("  ")))
+        first_element = elements[0]
+        if first_element.count(":") != 5:
+            wifi_inuse = True
+            elements.pop(0)
+
+        bssid = elements[0].strip()
+        name = elements[1].strip()
+        channel = elements[3].strip()
+        rate = elements[4].strip()
+        bars = elements[6].strip()
+        security = elements[7].strip()
+
+        if not wifi_inuse:
+            table.add_row(bssid, name, channel, rate, bars, security)
+        else:
+            table.add_row(bssid, name, channel, rate, bars, security, style="bold green")
+
+    console.print(table)
 
 @wifi_app.command("connect")
 def wifi_connect(ssid: str):
     out = run_shell_command(f"nmcli c up {ssid}")
-    print(out)
+    rich_print(out)
 
 @wifi_app.command("disconnect")
 def wifi_disconnect():
     connected_ssid = run_shell_command("iwgetid -r")
     out = run_shell_command(f"nmcli c down {connected_ssid}")
-    print(out)
+    rich_print(out)
 
 
 
@@ -58,9 +91,9 @@ def bluetooth_on():
         sleep(0.1)
         out = run_shell_command("bluetoothctl power on")
         if "Changing power on succeeded" in out:
-            print("Bluetooth power on succeeded!")
+            rich_print("[bold green]Bluetooth power on succeeded!")
             return
-    print("Bluetooth power on failed.")
+    rich_print("[bold red]Bluetooth power on failed.")
     
 @bluetooth_app.command("off")
 def bluetooth_off():
@@ -69,14 +102,14 @@ def bluetooth_off():
         sleep(0.1)
         out = run_shell_command("bluetoothctl power off")
         if "Changing power off succeeded" in out:
-            print("Bluetooth power off succeeded!")
+            rich_print("[bold green]Bluetooth power off succeeded!")
             return
-    print("Bluetooth power off failed.")
+    rich_print("[bold red]Bluetooth power off failed.")
 
 @bluetooth_app.command("list")
 def bluetooth_list():
     out = run_shell_command("bluetoothctl devices")
-    print(out)
+    rich_print(out)
 
 @bluetooth_app.command("connect")
 def bluetooth_connect(ssid: str):
@@ -90,13 +123,13 @@ def bluetooth_connect(ssid: str):
                 if line.startswith("\tConnected"):
                     connection_status = line.split(":")[1][1:]
                     if connection_status == "yes":
-                        print(f"Connected to {ssid}!")
+                        rich_print(f"[bold green]Connected to {ssid}!")
                         return
         except:
-            print(f"Connection timeout {i}, trying again...")
+            rich_print(f"Connection timeout {i}, trying again...")
             continue
-        print(f"Connection timeout {i}, trying again...")
-    print(f"Connection to {ssid} failed.")
+        rich_print(f"Connection timeout {i}, trying again...")
+    rich_print(f"[bold red]Connection to {ssid} failed.")
 
 @bluetooth_app.command("disconnect")
 def bluetooth_disconnect(ssid: str):
@@ -109,9 +142,9 @@ def bluetooth_pair_on():
         sleep(0.1)
         out = run_shell_command("bluetoothctl pairable on")
         if "Changing pairable on succeeded" in out:
-            print("Bluetooth pairable on succeeded!")
+            rich_print("[bold green]Bluetooth pairable on succeeded!")
             return
-    print("Bluetooth pairable on failed.")
+    rich_print("[bold red]Bluetooth pairable on failed.")
 
 @bluetooth_app.command("pair-off")
 def bluetooth_pair_off():
@@ -120,14 +153,14 @@ def bluetooth_pair_off():
         sleep(0.1)
         out = run_shell_command("bluetoothctl pairable off")
         if "Changing pairable off succeeded" in out:
-            print("Bluetooth pairable off succeeded!")
+            rich_print("[bold green]Bluetooth pairable off succeeded!")
             return
-    print("Bluetooth pairable off failed.")
+    rich_print("[bold red]Bluetooth pairable off failed.")
 
 @bluetooth_app.command("remove")
 def bluetooth_remove(ssid: str):
     out = run_shell_command(f"bluetoothctl remove {ssid}")
-    print(out)
+    rich_print(out)
 
 
 
